0d1b6a5181b0c5942ad0c3be9f04a68dee7cdc80
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-24 16:38:51 +0600

    Updated type

--- mweb_crud/helper/mweb_crud_callable.py
+++ mweb_crud/helper/mweb_crud_callable.py
@@ -1,10 +1,10 @@
 from typing import Protocol
-from mweb_orm import MWebBaseModel
+from mweb_orm import MWebBaseModel, MWebIDModel, MWebDatedModel, MWebModel
 
 
 class BeforeAfterSaveCallable(Protocol):
-    async def __call__(self, *, data: dict, model: MWebBaseModel) -> None: ...
+    async def __call__(self, *, data: dict, model: MWebModel | MWebBaseModel | MWebIDModel | MWebDatedModel) -> None: ...
 
 
 class BeforeAfterDeleteCallable(Protocol):
-    async def __call__(self, *, record_id: int, existing_model: MWebBaseModel) -> None: ...
+    async def __call__(self, *, record_id: int, existing_model: MWebModel | MWebBaseModel | MWebIDModel | MWebDatedModel) -> None: ...
